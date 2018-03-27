34ff53552929f6ec9f5417508b381a5994c3003c
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2018-03-27 21:57:30 +0100

    rename src -> include

--- API/index.php
+++ API/index.php
@@ -14,17 +14,17 @@ require __DIR__ . '/../vendor/autoload.php';
 session_start();
 
 // Instantiate the app
-$settings = require __DIR__ . 'src/settings.php';
+$settings = require __DIR__ . '/include/settings.php';
 $app = new \Slim\App($settings);
 
 // Set up dependencies
-require __DIR__ . 'src/dependencies.php';
+require __DIR__ . '/include/dependencies.php';
 
 // Register middleware
-require __DIR__ . 'src/middleware.php';
+require __DIR__ . '/include/middleware.php';
 
 // Register routes
-require __DIR__ . 'src/routes.php';
+require __DIR__ . '/include/routes.php';
 
 // Run app
 $app->run();
