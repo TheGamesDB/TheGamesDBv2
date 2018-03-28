52d3b84bf7a845e7cc1a501bf883802cae98c7bf
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2018-03-28 22:24:00 +0100

    Cleanup

--- API/include/routes.php
+++ API/include/routes.php
@@ -1,6 +1,6 @@
 <?php
-require __DIR__ . '/../../include/TGDB.API.php';
-require __DIR__ . '/Utils.class.php';
+require_once __DIR__ . '/../../include/TGDB.API.php';
+require_once __DIR__ . '/Utils.class.php';
 
 
 use Slim\Http\Request;
