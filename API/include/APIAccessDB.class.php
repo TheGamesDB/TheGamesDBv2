f8386ca48d3ea8b89d6e9c386d1fcacd4fec8269
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2018-03-31 01:57:16 +0100

    WIP/PoC: API access key

---
+++ API/include/APIAccessDB.class.php
@@ -0,0 +1,127 @@
+<?php
+require_once __DIR__ . "/../../include/db.config.php";
+
+class APIAccessDB
+{
+	private $database;
+	private $PlatformsTblCols;
+	private $GamesTblCols;
+
+	private function __construct()
+	{
+		$this->database = database::getInstance();
+	}
+
+	public static function getInstance()
+	{
+		static $instance = null;
+		if (!isset($instance))
+		{
+			$object = __CLASS__;
+			$instance = new $object;
+		}
+		return $instance;
+	}
+
+	function GetUserByAPIKey($key)
+	{
+
+		$dbh = $this->database->dbh;
+		$sth = $dbh->prepare("Select APIU.*, AA.monthly_allowance FROM apiusers APIU
+		LEFT JOIN api_allowance_level AA ON AA.id = APIU.api_allowance_level_id
+		WHERE apikey=:apikey;");
+		$sth->bindValue(':apikey', $key, PDO::PARAM_STR);
+
+		if($sth->execute())
+		{
+			$res = $sth->fetch(PDO::FETCH_OBJ | PDO::FETCH_GROUP);
+			return $res;
+		}
+	}
+
+	function GetUserByAPIKey_PLACEHOLDER($key)
+	{
+
+		$dbh = $this->database->dbh;
+		$sth = $dbh->prepare("Select APIU.*, AMC.id as c_id, AMC.count, AA.monthly_allowance FROM apiusers APIU
+		LEFT JOIN api_month_counter AMC ON APIU.id = AMC.apiusers_id AND AMC.month=:month AND AMC.year=:year
+		LEFT JOIN api_allowance_level AA ON AA.id = APIU.api_allowance_level_id
+		WHERE apikey=:apikey ;");
+
+		$sth->bindValue(':apikey', $key, PDO::PARAM_STR);
+		$sth->bindValue(':month', date("m"), PDO::PARAM_INT);
+		$sth->bindValue(':year', date("Y"), PDO::PARAM_INT);
+
+		if($sth->execute())
+		{
+			$res = $sth->fetch(PDO::FETCH_OBJ | PDO::FETCH_GROUP);
+			return $res;
+		}
+	}
+
+	function decreaseAllowanceCounter($User, $update_refresh_date)
+	{
+		try
+		{
+			$dbh = $this->database->dbh;			
+			if($update_refresh_date)
+			{
+				$sth = $dbh->prepare("UPDATE apiusers SET count = 1, last_refresh_date=:date WHERE id=:id;");
+				$sth->bindValue(':date', date('Y-m-d'));
+			}
+			else
+			{
+				$sth = $dbh->prepare("UPDATE apiusers SET count = count+1 WHERE id=:id;");			
+			}
+			$sth->bindValue(':id', $User->id, PDO::PARAM_INT);
+
+			$i = 1;
+			while(!$sth->execute())
+			{
+				if($i > 3)
+				{
+					break;
+				}
+				$i++;
+			}
+		}
+		catch (Exception $e)
+		{
+			//Free lunch :P
+		}
+	}
+
+	function increaseAllowanceCounter($User)
+	{
+		try
+		{		
+			$dbh = $this->database->dbh;
+			if(!empty($User->c_id))
+			{
+				$sth = $dbh->prepare("UPDATE api_month_counter SET count = count+1 WHERE id=:id;");
+				$sth->bindValue(':id', $User->c_id, PDO::PARAM_INT);
+			}
+			else
+			{
+				$sth = $dbh->prepare("INSERT INTO api_month_counter (apiusers_id, count, month, year)
+				VALUES (:apiusers_id, 1, :month, :year);");
+				$sth->bindValue(':apiusers_id', $User->id, PDO::PARAM_INT);
+				$sth->bindValue(':month', date("m"), PDO::PARAM_INT);
+				$sth->bindValue(':year', date("Y"), PDO::PARAM_INT);
+			}
+			$i = 1;
+			while(!$sth->execute())
+			{
+				if($i > 3)
+				{
+					break;
+				}
+				$i++;
+			}
+		}
+		catch (Exception $e)
+		{
+			//Free lunch :P
+		}
+	}
+}
--- API/include/middleware.php
+++ API/include/middleware.php
@@ -1,32 +1,58 @@
 <?php
 require_once __DIR__ . '/Utils.class.php';
+require_once __DIR__ . '/APIAccess.class.php';
 
 class AuthMiddleware
 {
-	//stub TODO
-	function valid_API_key($API_key)
-	{
-		return true;
-	}
-
 	public function __invoke($request, $response, $next)
 	{
 		if($_SERVER['PATH_INFO'] == '/')
 		{
 			return $next($request, $response);
 		}
-		else if(empty($_REQUEST['API']))
+		else if(empty($_REQUEST['apikey']))
 		{
 			$JSON_Response = Utils::getStatus(401);
 			return $response->withJson($JSON_Response, $JSON_Response['code']);
 		}
-		else if(!$this->valid_API_key($args['API']))
+		else
 		{
-			$JSON_Response = Utils::getStatus(403);
-			return $response->withJson($JSON_Response, $JSON_Response['code']);
+			$auth = APIAccessDB::getInstance();
+			$User = $auth->GetUserByAPIKey($_REQUEST['apikey']);
+			if(!empty($User))
+			{
+				$monthly_allowance = (!empty($User->monthly_allowance)) ? $User->monthly_allowance : 0;
+				$monthly_count = (!empty($User->count)) ? $User->count : 0;
+				if($update_refresh_date = strtotime($User->last_refresh_date) < strtotime('-30 days'))
+				{
+					$monthly_count = 0;
+				}
+				$remaining_monthly_allowance = $monthly_allowance - $monthly_count;
+				// TODO based on weather this will be a subscription based or allowance purchase
+				// 1) if subscription based we need to count 30 days instead of using start of the month
+				// 2) if allowance purchase, just countdown from their allowance
+				if($remaining_monthly_allowance > 0)
+				{
+					$response = $next($request, $response);
+					$auth->decreaseAllowanceCounter($User, $update_refresh_date);
+					$JSON_Response = json_decode($response->getBody(), true);
+					$JSON_Response['allowance'] = $remaining_monthly_allowance - 1;
+					return $response->withJson($JSON_Response, $JSON_Response['code']);
+				}
+				else
+				{
+					$JSON_Response = Utils::getStatus(403);
+					$JSON_Response['remaining_monthly_allowance'] = 0;
+					return $response->withJson($JSON_Response, $JSON_Response['code']);
+				}
+			}
+			else
+			{
+				$JSON_Response = Utils::getStatus(401);
+				$JSON_Response['remaining_monthly_allowance'] = 0;
+				return $response->withJson($JSON_Response, $JSON_Response['code']);
+			}
 		}
-
-		return $next($request, $response);
 	}
 }
 
