776632c671e52366bbb22b77fca42bc3b808b003
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2018-04-17 00:42:12 +0100

    add games.php page, with associated css/js and fancybox config

---
+++ website/game.php
@@ -0,0 +1,334 @@
+<?php
+require_once __DIR__ . "/include/ErrorPage.class.php";
+if(!isset($_REQUEST['id']) || !is_numeric($_REQUEST['id']))
+{
+	$errorPage = new ErrorPage();
+	$errorPage->SetHeader(ErrorPage::$HEADER_OOPS_ERROR);
+	$errorPage->SetMSG(ErrorPage::$MSG_MISSING_PARAM_ERROR);
+	$errorPage->print_die();
+}
+require_once __DIR__ . "/include/header.footer.class.php";
+include_once __DIR__ . "/include/TGDBUtils.class.php";
+include_once __DIR__ . "/../include/TGDB.API.php";
+
+if(isset($_REQUEST['id']) && !empty($_REQUEST['id']) && is_numeric($_REQUEST['id']))
+{
+	$limit = 20;
+	$page = Utils::getPage();
+	$offset = $page * $limit;
+	$options = array("Overview" => true, "Players" => true, "Rating" => true, "ESRB" => true, "boxart" => true, "coop" => true,
+		"Genre" => true, "Publisher" => true, "Platform" => true, "Youtube" => true);
+	$API = TGDB::getInstance();
+	$list = $API->GetGameByID($_REQUEST['id'], 0, 1, $options);
+	if(empty($list))
+	{
+		$errorPage = new ErrorPage();
+		$errorPage->SetHeader(ErrorPage::$HEADER_OOPS_ERROR);
+		$errorPage->SetMSG(ErrorPage::$MSG_REMOVED_GAME_INVALID_PARAM_ERROR);
+		$errorPage->print_die();
+	}
+	else
+	{
+		$Game = array_shift($list);
+		$covers = $API->GetGameBoxartByID($_REQUEST['id'], 0, 9999, 'ALL');
+		if(!empty($covers))
+		{
+			$Game->boxart = $covers[$_REQUEST['id']];
+		}
+	}
+	$Platform = $API->GetPlatforms($Game->Platform, array("icon" => true, "overview" => true, "developer" => true));
+	if(isset($Platform[$Game->Platform]))
+	{
+		$Platform = $Platform[$Game->Platform];
+	}
+}
+
+
+$fanarts = TGDBUtils::GetAllCovers($Game, 'fanart', '');
+$screenshots = TGDBUtils::GetAllCovers($Game, 'screenshot', '');
+$banners = TGDBUtils::GetAllCovers($Game, 'series', '');
+$is_graphics_empty = empty($fanarts) && empty($screenshots) && empty($banners);
+
+$box_cover =  new \stdClass();
+$box_cover->front = TGDBUtils::GetAllCovers($Game, 'boxart', 'front');
+if(!empty($box_cover->front))
+{
+	$box_cover->front = $box_cover->front[0];
+}
+$box_cover->back = TGDBUtils::GetAllCovers($Game, 'boxart', 'back');
+if(!empty($box_cover->back))
+{
+	$box_cover->back = $box_cover->back[0];
+}
+
+$Header = new HEADER();
+$Header->setTitle("TGDB - Browse - Game - $Game->GameTitle");
+$Header->appendRawHeader(function() { global $Game; ?>
+
+	<meta property="og:title" content="<?= $Game->GameTitle; ?>" />
+	<meta property="og:type" content="article" />
+	<meta property="og:image" content="<?= !empty($box_cover->front) ? $box_cover->front->thumbnail : "" ?>" />
+	<meta property="og:description" content="<?= htmlspecialchars($Game->Overview); ?>" />
+
+	<link href="/css/social-btn.css" rel="stylesheet">
+	<link href="/css/fontawesome.5.0.10.css" rel="stylesheet">
+	<link href="/css/fa-brands.5.0.10.css" rel="stylesheet">
+	<link href="/css/jquery.fancybox.min.3.3.5.css" rel="stylesheet">
+
+	<script type="text/javascript" defer src="/js/brands.5.0.10.js" crossorigin="anonymous"></script>
+	<script type="text/javascript" defer src="/js/fontawesome.5.0.10.js" crossorigin="anonymous"></script>
+
+	<script type="text/javascript" src="/js/jquery.fancybox.3.3.5.js"></script>
+	<script type="text/javascript" src="/js/fancybox.config.js"></script>
+
+	<script type="text/javascript">
+		$(document).ready(function()
+		{
+			fancyboxOpts.share.descr = function(instance, item)
+			{
+				return "<?= $Game->GameTitle ?>";
+			};
+			$('[data-fancybox]').fancybox(fancyboxOpts);
+		});
+	</script>
+	<style type="text/css">
+		.cover
+		{
+			width: 100%;
+			position: relative;
+		}
+		
+		@media screen and (min-width: 800px)
+		{
+			.cover-offset
+			{
+				margin-top: <?= isset($_REQUEST['test']) ? "-250px" : "-170px" ?>;
+			}
+			.fanart-banner
+			{
+				max-height: 100%;
+				height: <?= isset($_REQUEST['test']) ? "200px" : "325px" ?>;
+				overflow: hidden;
+				text-align: center;
+			}
+		}
+
+		@media screen and (max-width: 800px)
+		{
+			.cover-offset
+			{
+				margin-top: 0;
+			}
+
+			.fanart-banner
+			{
+				max-height: 100%;
+				height: 175px;
+				overflow: hidden;
+				text-align: center;
+			}
+		}
+	</style>
+<?php });?>
+<?= $Header->print(); ?>
+
+	<div class="container-fluid">
+
+		<div class="row" style="padding-bottom: 10px;">
+			<div class="col">
+				<div id="cover" class="view-width fanart-banner">
+				<?php if(!empty($cover = $fanarts) || !empty($cover = $screenshots)): ?>
+					<img class="cover cover-offset" src="<?= $cover[0]->medium ?>"/>
+				<?php else: ?>
+					<img class="cover" src="/images/placeholder_game_banner.jpg"/>
+				<?php endif; ?>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+
+			<div class="col-12 col-md-3 col-lg-2" style="padding-bottom:10px; text-align: center;">
+				<div class="row">
+					<div class="col">
+						<div class="card border-primary">
+							<?php if(!empty($box_cover->front)) : ?>
+							<a class="fancybox-thumb" data-fancybox="cover" data-caption="Front Cover" href="<?= $box_cover->front->original ?>">
+								<img class="card-img-top" src="<?= $box_cover->front->thumbnail ?>"/>
+							</a>
+								<?php if(!empty($box_cover->back)): ?>
+							<a class="fancybox-thumb" style="display:none;" data-fancybox="cover" data-caption="Back Cover"
+								href="<?= $box_cover->back->original ?>" data-thumb="<?= $box_cover->back->thumbnail ?>"/>
+							</a>
+								<?php endif; ?>
+								
+							<?php elseif(!empty($box_cover->back)): ?>
+							<a class="fancybox-thumb" data-fancybox="cover" data-caption="Back Cover" href="<?= $box_cover->front->original ?>">
+								<img class="card-img-top" src="<?= $box_cover->front->thumbnail ?>"/>
+							</a>
+							<?php else: ?>
+								<img class="card-img-top" src="<?= TGDBUtils::GetPlaceholderImage($Game->GameTitle, 'boxart'); ?>"/>
+							<?php endif; ?>
+							</a>
+								<div class="card-body">
+								<?php if(false) : ?>
+									<button type="button" data-toggle="bookmark" class="btn btn-danger btn-block btn-wrap-text">Remove From Collection <span class="glyphicon glyphicon-ok"></span></button>
+								<?php else: ?>
+									<button type="button" data-toggle="bookmark" class="btn btn-secondary btn-block btn-wrap-text">Add To Collection</button>
+								<?php endif;?>
+							</div>
+							<div class="card-body">
+								<?php if (!empty($Platform)) : ?>
+								<p>Platform: <a href="/platform.php?id=<?= $Platform->id?>"><?= $Platform->name; ?></a></p>
+								<?php endif; if (!empty($Platform)) : ?>
+								<p>Developer: <?= $Game->Developer; ?></p>
+								<?php endif; if (!empty($Game->Publisher)) : ?>
+								<p>Publisher: <?= $Game->Publisher; ?></p>
+								<?php endif; if (!empty($Game->ReleaseDate)) : ?>
+								<p>ReleaseDate: <?= $Game->ReleaseDate ;?></p>
+								<?php endif; if (!empty($Game->PlatformDetails)) : ?>
+								<p>Platform: <?= $Game->PlatformDetails->name; ?></p>
+								<?php endif; if (!empty($Game->Players)) : ?>
+								<p>Players: <?= $Game->Players; ?></p>
+								<?php endif; if (!empty($Game->coop)) : ?>
+								<p>Co-op: <?= $Game->coop; ?></p>
+								<?php endif ?>
+							</div>
+						</div>
+					</div>
+				</div>
+				<?php if (isset($login) && $login->isUserLoggedIn() && $_SESSION['user_access_level'] == 255) : ?>
+				<div class="row">
+					<div class="col">
+						<div class="card border-primary">
+							<div class="card-header">
+								Extras
+							</div>
+							<div class="card-body">
+								<?php if (isset($Game->id)) : ?>
+								TGDB ID: <?= $Game->id; ?><br/>
+								<?php endif; ?>
+							</div>
+						</div>
+					</div>
+				</div>
+				<?php endif;?>
+			</div>
+
+			<div class="col-12 col-md-9 col-lg-8">
+				<div class="row" style="padding-bottom:10px">
+					<div class="col">
+						<div class="card border-primary">
+							<div class="card-header">
+								<h1><?= $Game->GameTitle;?></h1>
+							</div>
+							<div class="card-body">
+								<p><?= !empty($Game->Overview) ? $Game->Overview : "No overview is currently available for this title, please feel free to add one.";?></p>
+								<?php if (!empty($Game->Youtube)) : ?>
+								<p>Trailer: <a data-fancybox data-caption="Trailer" href="https://youtube.com/watch?v=<?= $Game->Youtube?>">YouTube</a></p>
+								<?php endif;?>
+							</div>
+							<div class="card-footer" style="text-align: center;">
+								<p>Share Via</p>
+								<!-- Twitter -->
+								<div data="https://twitter.com/intent/tweet?text=<?= urlencode("Checkout '$Game->GameTitle' on ")."&amp;url=".urlencode( "https://thegamesdb.net.net/game.php?id=$Game->id");?>" onclick="javascript:window.open(this.attributes['data'].value, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=700');return false;" title="Share on Twitter" target="_blank" class="btn btn-twitter">
+									<i class="fab fa-twitter"></i> Twitter
+								</div>
+								<!-- Facebook -->
+								<div data="https://www.facebook.com/sharer/sharer.php?u=<?= urlencode( "https://thegamesdb.net.net/game.php?id=$Game->id");?>" title="Share on Facebook" onclick="javascript:window.open(this.attributes['data'].value, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=700');return false;" target="_blank" class="btn btn-facebook">
+									<i class="fab fa-facebook"></i> Facebook
+								</div>
+								<!-- Google+ -->
+								<div data="https://plus.google.com/share?url=<?= urlencode( "https://thegamesdb.net.net/game.php?id=$Game->id");?>" title="Share on Google+" onclick="javascript:window.open(this.attributes['data'].value, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=600');return false;" target="_blank" class="btn btn-googleplus">
+									<i class="fab fa-google-plus"></i> Google+
+								</div>
+								<!-- StumbleUpon -->
+								<div data="http://www.stumbleupon.com/submit?url=<?= urlencode( "https://thegamesdb.net.net/game.php?id=$Game->id");?>" title="Share on StumbleUpon" onclick="javascript:window.open(this.attributes['data'].value, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=700');return false;" target="_blank" data-placement="top" class="btn btn-stumbleupon">
+									<i class="fab fa-stumbleupon"></i> Stumbleupon
+								</div>
+								<!-- Pinterest -->
+								<div data="https://www.pinterest.com/pin/create/button/?description=<?= urlencode("Checkout '$Game->GameTitle' on https://thegamesdb.net.net/game.php?id=$Game->id")."&amp;url=".urlencode( "https://thegamesdb.net.net/game.php?id=$Game->id");?>&media=<?= !empty($box_cover->front) ? urlencode($box_cover->front->thumbnail) : "" ?>" title="Share on Pinterest" onclick="javascript:window.open(this.attributes['data'].value, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=700');return false;" target="_blank" data-placement="top" class="btn btn-pinterest">
+									<i class="fab fa-pinterest"></i> Pinterest
+								</div>
+							</div>
+						</div>
+						<?php if(isset($kitsu) && $kitsu->isValid()) : ?>
+						<br/>
+						<div class="card border-success">
+							<div class="panel panel-default">
+								<div class="card-header">
+									<a data-toggle="collapse" href="#collapse1">Kitsu panel</a>
+								</div>
+								<div  class="ok">
+									<div id="collapse1" class="card-body collapse" style="background:white;">
+										<pre><?php print_r($kitsu->getJSON()); ?></pre>
+									</div>
+								</div>
+							</div>
+						</div>
+						<?php endif;?>
+					</div>
+				</div>
+
+				<?php if (true) : ?>
+				<div class="row" style="padding-bottom:10px;">
+					<div class="col">
+						<div class="card border-primary">
+							<h3  class="card-header">
+								Other Graphic(s)
+							</h3>
+							<div class="card-body">
+								<div class="row justify-content-center">
+									<?php if(!empty($cover = array_shift($fanarts))) : ?>
+									<div class="col-12 col-sm-6" style="margin-bottom:10px; overflow:hidden;">
+										<a class="fancybox-thumb" data-fancybox="fanarts" data-caption="Fanart" href="<?= $cover->original ?>">
+											<img class="rounded img-thumbnail img-fluid" src="<?= $cover->cropped_center_thumb ?>" alt=""/>
+											<img src="/images/ribbonFanarts.png" style="position: absolute; left: 15px; top: 0; height: 80%; z-index: 10"/>
+										</a>
+										<?php while($cover = array_shift($fanarts)) : ?>
+											<a class="fancybox-thumb" style="display:none" data-fancybox="fanarts" data-caption="Fanart"
+												href="<?= $cover->original ?>" data-thumb="<?= $cover->thumbnail ?>"></a>
+										<?php endwhile; ?>
+									</div>
+									<?php endif; ?>
+									<?php if(!empty($cover = array_shift($screenshots))) : ?>
+									<div class="col-12 col-sm-6" style="margin-bottom:10px; overflow:hidden;">
+										<a class="fancybox-thumb" data-fancybox="screenshots" data-caption="Screenshot" href="<?= $cover->original ?>">
+											<img class="rounded img-thumbnail img-fluid" src="<?= $cover->cropped_center_thumb ?>"/>
+											<img src="/images/ribbonScreens.png" style="position: absolute; left: 15px; top: 0; height: 80%; z-index: 10"/>
+										</a>
+										<?php while($cover = array_shift($screenshots)) : ?>
+											<a class="fancybox-thumb" style="display:none" data-fancybox="screenshots" data-caption="Screenshot"
+												href="<?= $cover->original ?>" data-thumb="<?= $cover->thumbnail ?>"></a>
+										<?php endwhile; ?>
+									</div>
+									<?php endif; ?>
+
+									<?php if(!empty($cover = array_shift($banners))) : ?>
+									<div class="col-12" style="margin-bottom:10px; overflow:hidden;">
+										<a class="fancybox-thumb" data-fancybox="banners" data-caption="Banner" href="<?= $cover->original ?>">
+											<img class="rounded img-thumbnail img-fluid" src="<?= $cover->thumbnail ?>"/>
+											<img src="/images/ribbonBanners.png" style="position: absolute; left: 15px; top: 0; height: 80%; z-index: 10"/>
+										</a>
+										<?php while($cover = array_shift($banners)) : ?>
+											<a class="fancybox-thumb" style="display:none" data-fancybox="banners" data-caption="Banner"
+												href="<?= $cover->original ?>" data-thumb="<?= $cover->thumbnail ?>"></a>
+										<?php endwhile; ?>
+									</div>
+									<?php endif; ?>
+									<?php if($is_graphics_empty) : ?>
+									<h5>No fanarts/screenshots/banners found, be the 1st to add them.</h5>
+									<?php endif; ?>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<?php endif; ?>
+
+			</div>
+
+		</div>
+	</div>
+
+<?php FOOTER::print(); ?>
---
+++ website/js/fancybox.config.js
@@ -0,0 +1,53 @@
+
+fancyboxOpts =
+{
+	buttons:
+	[
+		"zoom",
+		"share",
+		"slideShow",
+		"fullScreen",
+		"download",
+		"thumbs",
+		"close"
+	],
+	slideShow:
+	{
+		autoStart: false,
+		speed: 4000
+	},
+	thumbs:
+	{
+		autoStart: true,
+		hideOnClose: true,
+		axis: "x"
+	},
+	share:
+	{
+		tpl:
+			'<div class="fancybox-share">' +
+			"<h1>{{SHARE}}</h1>" +
+			"<p>" +
+			'<a class="fancybox-share__button fancybox-share__button--tw" href="https://twitter.com/intent/tweet?url={{url}}&text={{descr}} title="Share on Twitter"" target="_blank">' +
+			'<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m456 133c-14 7-31 11-47 13 17-10 30-27 37-46-15 10-34 16-52 20-61-62-157-7-141 75-68-3-129-35-169-85-22 37-11 86 26 109-13 0-26-4-37-9 0 39 28 72 65 80-12 3-25 4-37 2 10 33 41 57 77 57-42 30-77 38-122 34 170 111 378-32 359-208 16-11 30-25 41-42z" /></svg>' +
+			"<span>Twitter</span>" +
+			"</a>" +
+			'<a class="fancybox-share__button fancybox-share__button--fb" href="https://www.facebook.com/sharer/sharer.php?u={{url}} title="Share on Facebook"" target="_blank">' +
+			'<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m287 456v-299c0-21 6-35 35-35h38v-63c-7-1-29-3-55-3-54 0-91 33-91 94v306m143-254h-205v72h196" /></svg>' +
+			"<span>Facebook</span>" +
+			"</a>" +
+			'<a class="fancybox-share__button btn-googleplus" href="https://plus.google.com/share?url={{url}}" title="Share on Google+" target="_blank">' +
+			'<i class="fab fa-google-plus"></i> Google+' +
+			'</a>' +
+			'<a class="fancybox-share__button btn-stumbleupon" href="http://www.stumbleupon.com/submit?url={{url}}" title="Share on StumbleUpon" target="_blank">' +
+			'<i class="fab fa-stumbleupon"></i> Stumbleupon' +
+			"</a>" +
+			'<a class="fancybox-share__button fancybox-share__button--pt" href="https://www.pinterest.com/pin/create/button/?url={{url}}&description={{descr}}%20{{url}}&media={{media}}" title="Share on Pinterest">' +
+			'<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m265 56c-109 0-164 78-164 144 0 39 15 74 47 87 5 2 10 0 12-5l4-19c2-6 1-8-3-13-9-11-15-25-15-45 0-58 43-110 113-110 62 0 96 38 96 88 0 67-30 122-73 122-24 0-42-19-36-44 6-29 20-60 20-81 0-19-10-35-31-35-25 0-44 26-44 60 0 21 7 36 7 36l-30 125c-8 37-1 83 0 87 0 3 4 4 5 2 2-3 32-39 42-75l16-64c8 16 31 29 56 29 74 0 124-67 124-157 0-69-58-132-146-132z" fill="#fff"/></svg>' +
+			"<span>Pinterest</span>" +
+			'</a>' +
+			"</p>" +
+			'<p><input class="fancybox-share__input" type="text" value="{{url_raw}}" /></p>' +
+			"</div>"
+	}
+};
