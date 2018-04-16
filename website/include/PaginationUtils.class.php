5cd23b6a62489ad895c3e94ce1470c226929b405
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2018-04-17 00:08:04 +0100

    Add PaginationUtils.class.php

---
+++ website/include/PaginationUtils.class.php
@@ -0,0 +1,39 @@
+<?php
+
+class PaginationUtils
+{
+
+	static function getPage()
+	{
+		if(!empty($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] > 0)
+		{
+			return $_GET['page'];
+		}
+		return 1;
+	}
+
+	public static function Create($has_next_page)
+	{
+		$page = PaginationUtils::getPage();
+		{
+			$GET = $_GET;
+			$GET['page'] = $page - 1;
+			$previous_args = "?" . http_build_query($GET,'','&');
+
+			$GET['page'] = $page + 1;
+			$next_args = "?" . http_build_query($GET,'','&');
+			?>
+			<nav aria-label="Page navigation example">
+				<ul class="pagination justify-content-center">
+					<li class="page-item <?= $page <= 1 ? "disabled" : ""?>">
+						<a class="page-link" href="<?= $page > 1 ? $previous_args : "#"?>" tabindex="-1">&lt;&lt; Previous</a>
+					</li>
+					<li class="page-item <?= !$has_next_page ? "disabled" : ""?>">
+						<a class="page-link" href="<?= $has_next_page > 0 ? $next_args : "#"?>">Next &gt;&gt;</a>
+					</li>
+				</ul>
+			</nav>
+			<?php
+		}
+	}
+}
