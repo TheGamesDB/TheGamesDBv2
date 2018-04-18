6f15a13ac8a0caba89fe812c8aef39c892fa9097
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2018-04-18 22:29:03 +0100

    use CDN for image loading

--- API/include/Utils.class.php
+++ API/include/Utils.class.php
@@ -3,7 +3,7 @@
 class Utils
 {
 	static private $API_BASE_URL = "https://api.thegamesdb.net";
-	static public $BOXART_BASE_URL = "http://thegamesdb.net/banners/";
+	static public $BOXART_BASE_URL = "https://cdn.thegamesdb.net/images/";
 	static public $BOXART_CACHE_BASE_URL = "http://thegamesdb.net/banners/_cache/";
 
 	static private $_statusMSG = array(
