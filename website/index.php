d82de23cd9a98678f258e23bdbcb61d2b3a5d972
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2018-06-24 19:35:27 +0300

    Change the way front page recently released images are displayed

--- website/index.php
+++ website/index.php
@@ -58,8 +58,8 @@ $Header->appendRawHeader(function() { ?>
 				<div class="row">
 					<?php foreach($recent as $game) : ?>
 					<div class="col-6 col-md-2">
-						<a href="/game.php?id=<?= $game->id ?>" class="crop">
-							<span class="img" style="background-image:url('<?= TGDBUtils::GetCover($game, 'boxart', 'front', true,  true, 'thumb') ?>');">
+						<a href="/game.php?id=<?= $game->id ?>" style="padding-bottom: 10px;">
+							<img class="cover-overlay" src="<?= TGDBUtils::GetCover($game, 'boxart', 'front', true,  true, 'cropped_center_thumb_square') ?>">
 							<div class="cover-text-col-3 cover-text cover-text-bottom cover-text-hover"><?= $game->GameTitle ?></div>
 						</a>
 					</div>
