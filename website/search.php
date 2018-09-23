f3400a2329532f630bf258dfaaeb9a1bd09911d0
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2018-09-23 13:00:28 +0100

    cleanup: useful alt

--- website/search.php
+++ website/search.php
@@ -96,7 +96,7 @@ $Header->setTitle("TGDB - Search");
 				<div style="padding-bottom:12px; height: 100%">
 					<a href="./game.php?id=<?= $Game->id ?>">
 						<div class="card border-primary" style="height: 100%">
-							<img class="card-img-top" alt="PosterIMG" src="<?= TGDBUtils::GetCover($Game, 'boxart', 'front', true, true, 'thumb') ?>">
+						<img class="card-img-top" alt="<?= $Game->game_title ?> cover" src="<?= TGDBUtils::GetCover($Game, 'boxart', 'front', true, true, 'thumb') ?>">
 							<div class="card-body card-noboday" style="text-align:center;">
 							</div>
 							<div class="card-footer bg-secondary" style="text-align:center;">
