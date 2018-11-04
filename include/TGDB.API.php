f2a544c91b5c2cb6d75675dea0effdaa5d49480b
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2018-11-04 22:08:01 +0000

    Fix typo

--- include/TGDB.API.php
+++ include/TGDB.API.php
@@ -1555,7 +1555,7 @@ class TGDB
 		return $sth->execute();
 	}
 
-	function UpdateGamesAltName($user, $games_id, $new_alt_names)
+	function UpdateGamesAltName($user_id, $games_id, $new_alt_names)
 	{
 		$dbh = $this->database->dbh;
 
