f52bb03e2d1675cceb6b9bb420d15e55fc28d951
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2020-02-11 12:48:45 +0000

    add api limit endpoint

--- API/include/routes.php
+++ API/include/routes.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../../include/TGDB.API.php';
 require_once __DIR__ . '/../../include/CommonUtils.class.php';
 require_once __DIR__ . '/Utils.class.php';
-
+require_once __DIR__ . '/APIAccessDB.class.php';
 
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -699,5 +699,56 @@ $app->group('/v1', function()
 		});
 	});
 })->add(new AuthMiddleware());
+
+$app->get('/v1/API/Limit', function($request, $response, $args)
+{
+	$this->logger->info("TGDB '/v1/API/Limit' route");
+	$auth = APIAccessDB::getInstance();
+	$User = $auth->GetUserAllowanceByAPIKey($_REQUEST['apikey']);
+	if(empty($_REQUEST['apikey']))
+	{
+		$JSON_Response = Utils::getStatus(401);
+		return $response->withJson($JSON_Response, $JSON_Response['code']);
+	}
+	else
+	{
+		if(!empty($User))
+		{
+			$monthly_allowance = (!empty($User->monthly_allowance)) ? $User->monthly_allowance : 0;
+			$monthly_count = (!empty($User->count)) ? $User->count : 0;
+			$refresh = strtotime("+30 days", strtotime($User->last_refresh_date)) - time();
+			if($update_refresh_date = $refresh < 0)
+			{
+				$refresh = strtotime("+30 days", 0);
+				$monthly_count = 0;
+			}
+			$remaining_monthly_allowance = $monthly_allowance - $monthly_count;
+			if($remaining_monthly_allowance > 0 || $User->extra_allowance > 0)
+			{
+				$use_extra = $remaining_monthly_allowance <= 0;
+
+				$JSON_Response = [];
+				$JSON_Response['remaining_monthly_allowance'] = $remaining_monthly_allowance + (!$use_extra ? -1 : 0);
+				$JSON_Response['extra_allowance'] =  $User->extra_allowance + ($use_extra ? -1 : 0);
+				$JSON_Response['allowance_refresh_timer'] = ($User->is_private_key == 1) ? NULL : $refresh;
+				return $response->withJson($JSON_Response, 200);
+			}
+			else
+			{
+				$JSON_Response = [];
+				$JSON_Response['remaining_monthly_allowance'] = 0;
+				$JSON_Response['allowance_refresh_timer'] = ($User->is_private_key == 1) ? NULL : $refresh;
+				return $response->withJson($JSON_Response, 200);
+			}
+		}
+		else
+		{
+			$JSON_Response = [];
+			$JSON_Response['remaining_monthly_allowance'] = 0;
+			$JSON_Response['allowance_refresh_timer'] = 0;
+			return $response->withJson($JSON_Response, 200);
+		}
+	}
+});
 
 ?>
