ddec8c4584ecc497c0567dde861b00ffa9d0a9f1
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2020-03-15 13:49:30 +0000

    devs/pubs merge interface

---
+++ website/actions/merge_dev_pub.php
@@ -0,0 +1,166 @@
+<?php
+require_once __DIR__ . "/../include/ErrorPage.class.php";
+require_once __DIR__ . "/../include/login.phpbb.class.php";
+
+function returnJSONAndDie($code, $msg)
+{
+	echo json_encode(array("code" => $code, "msg" => $msg));
+	die();
+}
+
+$_user = phpBBuser::getInstance();
+if(!$_user->isLoggedIn())
+{
+	returnJSONAndDie(-1, ErrorPage::$MSG_NOT_LOGGED_IN_EDIT_ERROR);
+}
+else
+{
+	if(!$_user->hasPermission('m_delete_games'))
+	{
+		returnJSONAndDie(-1, ErrorPage::$MSG_NO_PERMISSION_TO_EDIT_ERROR);
+	}
+}
+
+if(
+    !isset($_REQUEST['keep']) || !is_numeric($_REQUEST['keep']) ||
+    !isset($_REQUEST['remove']) || !is_numeric($_REQUEST['remove'])
+)
+{
+	returnJSONAndDie(-1, ErrorPage::$MSG_MISSING_PARAM_ERROR);
+}
+
+if($_REQUEST['remove'] == $_REQUEST['keep'])
+{
+	returnJSONAndDie(-1, "Invalid Selection");
+}
+
+require_once __DIR__ . "/../../include/db.config.php";
+require_once __DIR__ . "/../../include/TGDB.API.php";
+
+$database = database::getInstance();
+$API = TGDB::getInstance();
+
+$dbh = $database->dbh;
+
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+
+$old_id = $_REQUEST['remove'];
+$new_id = $_REQUEST['keep'];
+$is_pub = $_REQUEST['tbl'] == 'publishers';
+
+if($is_pub)
+{
+    $games_tbl = "games_pubs";
+    $list_tbl = "pubs_list";
+    $field = "pub_id";
+    $edit_name = "publishers";
+    if(count($API->GetPubsListByIDs([$old_id, $new_id])) != 2)
+    {
+        returnJSONAndDie(-2, "1 or both $edit_name can't be found.");
+    }
+}
+else
+{
+    $games_tbl = "games_devs";
+    $list_tbl = "devs_list";
+    $field = "dev_id";
+    $edit_name = "developers";
+    if(count($API->GetDevsListByIDs([$old_id, $new_id])) != 2)
+    {
+        returnJSONAndDie(-2, "1 or both $edit_name can't be found.");
+    }
+}
+
+try
+{
+    // 1) find all games with pub/dev id
+    $sth = $dbh->prepare("SELECT games_id from $games_tbl where $field = :$field;");
+    $sth->bindValue(":$field", $old_id);
+    if($sth->execute())
+    {
+        $Games = $sth->fetchAll(PDO::FETCH_OBJ);
+    }
+    if(empty($Games))
+    {
+        returnJSONAndDie(-3, "Error No Games found under $edit_name");
+    }
+
+    $dbh->beginTransaction();
+    // 2) delete games dev/pub
+    $sth = $dbh->prepare("DELETE FROM $games_tbl where $field = :$field;");
+    $sth->bindValue(":$field", $old_id);
+    $sth->execute();
+
+    // 3) delete pub/dev listing
+    $sth = $dbh->prepare("DELETE FROM $list_tbl where id = :$field;");
+    $sth->bindValue(":$field", $old_id);
+    $sth->execute();
+
+    // 4) updating all games listing
+    $insert_values = '';
+    foreach($Games as $Game)
+    {
+        if(!empty($insert_values))
+        {
+            $insert_values .= ",";
+        }
+    
+        $insert_values .= "(" . $Game->games_id . ", $new_id) ";
+    }
+
+    $sth = $dbh->prepare("INSERT INTO  $games_tbl (games_id, $field) values $insert_values;");
+    $sth->execute();
+    
+    if(!$dbh->commit())
+    {
+        $dbh->rollBack();
+        echo "fail!!!";
+        echo "INSERT INTO  $games_tbl (games_id, $field) values $insert_values;";
+        die();
+    }
+}
+catch(Exception $e)
+{
+    $dbh->rollBack();
+    echo 'Message: ' .$e->getMessage();
+    echo "INSERT INTO  $games_tbl (games_id, $field) values $insert_values;";
+    die();
+}
+
+foreach($Games as $Game)
+{
+    $ids[] = $Game->games_id;
+}
+
+if($is_pub)
+{
+    $ids = $API->GetGamesPubs($ids);
+}
+else
+{
+    $ids = $API->GetGamesDevs($ids);
+}
+
+ob_start();
+foreach($Games as $Game)
+{
+    $valid_ids = [];
+    if(isset($ids[$Game->games_id]))
+    {
+        foreach($ids[$Game->games_id] as $subval)
+        {
+            $valid_ids[] = $subval;
+        }
+        if($API->InsertUserEdits(48, $Game->games_id, $edit_name, json_encode($valid_ids, JSON_NUMERIC_CHECK)))
+        {
+            echo "game_id: $Game->games_id\n";
+            print_r($valid_ids);
+            echo "Phase 2 success!!!\n";
+        }
+    }
+}
+$output = ob_get_clean();
+returnJSONAndDie(0, $output);
+
---
+++ website/merge_dev_pub.php
@@ -0,0 +1,193 @@
+<?php
+require_once __DIR__ . "/include/ErrorPage.class.php";
+require_once __DIR__ . "/include/login.phpbb.class.php";
+$_user = phpBBUser::getInstance();
+if(!$_user->isLoggedIn())
+{
+	$errorPage = new ErrorPage();
+	$errorPage->SetHeader(ErrorPage::$HEADER_OOPS_ERROR);
+	$errorPage->SetMSG(ErrorPage::$MSG_NOT_LOGGED_IN_EDIT_ERROR);
+	$errorPage->print_die();
+}
+else
+{
+	if(!$_user->hasPermission('m_delete_games'))
+	{
+		$errorPage = new ErrorPage();
+		$errorPage->SetHeader(ErrorPage::$HEADER_OOPS_ERROR);
+		$errorPage->SetMSG(ErrorPage::$MSG_NO_PERMISSION_TO_EDIT_ERROR);
+		$errorPage->print_die();
+	}
+}
+
+require_once __DIR__ . "/include/header.footer.class.php";
+require_once __DIR__ . "/include/TGDBUtils.class.php";
+require_once __DIR__ . "/../include/TGDB.API.php";
+require_once __DIR__ . "/../include/CommonUtils.class.php";
+
+$API = TGDB::getInstance();
+$devs_list = $API->GetDevsList();
+$pubs_list = $API->GetPubsList();
+
+$Header = new HEADER();
+$Header->setTitle("TGDB - Merge");
+$Header->appendRawHeader(function() { global $_user, $devs_list, $pubs_list; ?>
+
+	<link href="/css/select-pure.css" rel="stylesheet">
+	<link href="/css/social-btn.css" rel="stylesheet">
+	<link href="/css/fontawesome.5.0.10.css" rel="stylesheet">
+	<link href="/css/fa-brands.5.0.10.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+	<link href="/css/jquery.fancybox.min.3.3.5.css" rel="stylesheet">
+
+	<script type="text/javascript" defer src="/js/brands.5.0.10.js" crossorigin="anonymous"></script>
+	<script type="text/javascript" defer src="/js/fontawesome.5.0.10.js" crossorigin="anonymous"></script>
+
+	<script type="text/javascript" src="/js/jquery.fancybox.3.3.5.js"></script>
+	<script type="text/javascript" src="/js/fancybox.config.js"></script>
+	<script type="text/javascript" src="https://unpkg.com/select-pure@latest/dist/bundle.min.js"></script>
+
+	<script type="text/javascript">
+		function isJSON(json)
+		{
+			try
+			{
+				return (JSON.parse(json) && !!json);
+			}
+			catch (e)
+			{
+				return false;
+			}
+		}
+		$(document).ready(function()
+		{
+			const multi_devs_selection = [
+				<?php foreach($devs_list as $dev) : ?> { label: "<?= $dev->name ?> (ID: <?= $dev->id ?>)", value: "<?= $dev->id ?>" },<?php endforeach; ?>
+			];
+
+			keep_multi_devs = new SelectPure('#devs_list_keep', {
+				options: multi_devs_selection,
+				autocomplete: true,
+				multiple: false,
+				icon: "fas fa-times",
+			});
+			remove_multi_devs = new SelectPure('#devs_list_remove', {
+				options: multi_devs_selection,
+				autocomplete: true,
+				multiple: false,
+				icon: "fas fa-times",
+			});
+
+			const multi_pubs_selection = [
+				<?php foreach($pubs_list as $pub) : ?> { label: "<?= $pub->name ?>(ID: <?= $pub->id ?>)", value: "<?= $pub->id ?>" },<?php endforeach; ?>
+			];
+			keep_multi_pubs = new SelectPure('#pubs_list_keep', {
+				options: multi_pubs_selection,
+				autocomplete: true,
+				multiple: false,
+				icon: "fas fa-times",
+			});
+			remove_multi_pubs = new SelectPure('#pubs_list_remove', {
+				options: multi_pubs_selection,
+				autocomplete: true,
+				multiple: false,
+				icon: "fas fa-times",
+			});
+			
+
+
+			<?php if($_user->hasPermission('m_delete_games')) : ?>
+				function getInput(tbl_name, keep, remove, var_url)
+				{
+					return {
+						type: "POST",
+						url: "./actions/merge_dev_pub.php",
+						data: "tbl=" + tbl_name + "&keep=" + keep + "&remove=" + remove,
+						success: function(data)
+						{
+							if(isJSON(data))
+							{
+								var obj = JSON.parse(data);
+								if(obj.code == -2)
+								{
+									alert("selected " + tbl_name + " don't exist, reloading page.");
+									location.reload(true);
+								}
+								else if(obj.code == 0)
+								{
+									alert("Success")
+									location.reload(true);
+								}
+								else
+								{
+									alert(data);
+								}
+								return;
+							}
+							else
+							{
+								alert("Error: Something Unexpected Happened.")
+								return;
+							}
+						}
+					};
+				}
+				$("#dev_form").submit(function(e)
+				{
+					var keep = keep_multi_devs._config.value;
+					var remove = remove_multi_devs._config.value;
+					var tbl_name = "developers";
+					var input = getInput(tbl_name, keep, remove);
+					$.ajax(input);
+
+					e.preventDefault();
+				});
+				$("#pub_form").submit(function(e)
+				{
+					var keep = keep_multi_pubs._config.value;
+					var remove = remove_multi_pubs._config.value;
+					var tbl_name = "publishers";
+					var input = getInput(tbl_name, keep, remove);
+					$.ajax(input);
+
+					e.preventDefault();
+				});
+			<?php endif;?>
+		});
+	</script>
+<?php });?>
+<?= $Header->print(); ?>
+
+		<form id="dev_form" class="row justify-content-center">
+			<div class="col-6">
+				<h2>Developer</h2>
+				<div class="card-footer">
+					<h4>Keep</h4>
+					<p><span id="devs_list_keep"></span></p>
+				</div>
+				<div class="card-footer">
+					<h4>Remove</h4>
+					<p><span id="devs_list_remove"></span></p>
+				</div>
+				<p><button type="submit" class="btn btn-primary btn-block">Merge</button></p>
+			</div>
+		</form>
+		<form id="pub_form" class="row justify-content-center">
+			<div class="col-6">
+				<h2>Publisher</h2>
+
+				<div class="card-footer">
+					<h4>Keep</h4>
+					<p><span id="pubs_list_keep"></span></p>
+				</div>
+				<div class="card-footer">
+					<h4>Remove</h4>
+					<p><span id="pubs_list_remove"></span></p>
+				</div>
+				<p><button type="submit" class="btn btn-primary btn-block">Merge</button></p>
+			</div>
+		</form>
+	</form>
+
+<?php FOOTER::print(); ?>
+
