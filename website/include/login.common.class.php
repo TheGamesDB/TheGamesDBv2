10c356fecea71aa3e7c310f8b533bfe761b7df32
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2021-06-05 11:39:51 +0100

    use phpbb login

--- website/include/login.common.class.php
+++ website/include/login.common.class.php
@@ -2,7 +2,7 @@
 
 // enable for testing
 // this will allow you to have a pseudo login tp avoid the need to setup phpbb forum
-if(true)
+if(false)
 {
 	require __DIR__ . "/login.pseudo.class.php";
 }
