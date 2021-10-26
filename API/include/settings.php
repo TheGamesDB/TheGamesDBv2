0acc64fe34cd42359a8320264aeab663e41e673c
Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2021-10-26 12:26:34 +0100

    API: only log errors load

--- API/include/settings.php
+++ API/include/settings.php
@@ -13,7 +13,7 @@ return [
 		'logger' => [
 			'name' => 'slim-app',
 			'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
-			'level' => \Monolog\Logger::DEBUG,
+			'level' => \Monolog\Logger::ERROR,
 		],
 	],
 ];
